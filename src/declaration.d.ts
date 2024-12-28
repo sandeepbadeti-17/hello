a78e25dbb5bdcc360a19dd9378edf42142c74d0e
SANDEEP BADETI <sandeepbadeti7730@gmail.com>
2024-12-28 23:40:58 +0530

    first commit

---
+++ src/declaration.d.ts
@@ -0,0 +1,9 @@
+declare module '*.css' {
+    const content: { [className: string]: string };
+    export default content;
+  }
+declare module "node";
+declare module 'vite';
+declare module 'vite/client';
+declare module '@vitejs/plugin-react';
+declare module 'react-dom/client';
